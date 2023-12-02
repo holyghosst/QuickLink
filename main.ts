b1fcfc289f61d3f7fa7673128d26ad2a0a0bbf82
holyghosst <109752893+holyghosst@users.noreply.github.com>
2023-12-02 15:56:30 +0100

    added plugin settings

--- main.ts
+++ main.ts
@@ -1,19 +1,71 @@
-import {Plugin, Editor} from "obsidian"
+import {App, Plugin, Editor, PluginSettingTab, Setting} from "obsidian"
+import { delimiter } from "path";
+import { text } from "stream/consumers";
+
+interface QuickLinkSettings {
+	delimiter_char: string;
+}
+
+const DEFAULT_SETTINGS: Partial<QuickLinkSettings> = {
+	delimiter_char: "_"
+};
 
 export default class QuickLinkPlugin extends Plugin{
-	onload(): void{
+	settings : QuickLinkSettings
+	async onload() {
+		await this.loadSettings()
+		
+		this.addSettingTab(new QuickLinkSettingTab(this.app, this));
+
+		let delimiter = this.settings.delimiter_char
 		this.addCommand({
 			id: "add-quick-link",
 			name: "Add quick link",
 			editorCallback(editor: Editor) {
 				const link = editor.getSelection();
-				const link_array = link.split("_")
+				const link_array = link.split(delimiter)
 				editor.replaceSelection("[[" + link_array[0] + "|" + link_array[1] + "]]");
 			}
 		});
-	};
+	}
 
-	unload(): void {
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+	async unload() {
+	}
+};
+
+export class QuickLinkSettingTab extends PluginSettingTab{
+	plugin: QuickLinkPlugin;
+
+	constructor(app: App, plugin: QuickLinkPlugin) {
+		super(app, plugin);
+		this.plugin = plugin
+	}
+
+
+	display() {
+		let {containerEl} = this;
 		
-	};
+		containerEl.empty();
+
+		new Setting(containerEl)
+		.setName("String delimiter")
+		.setDesc("Default delimiter")
+		.addText((text) =>
+			text
+			.setValue(this.plugin.settings.delimiter_char)
+			.onChange(async (value) => {
+				this.plugin.settings.delimiter_char = value;
+				await this.plugin.saveSettings();
+			})
+		)}
+
 };
